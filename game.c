0eadcaa9cb5f7e86696d5dc5d7bede80afd2b063
AchariNehal <nehalachari25@gmail.com>
2026-03-22 18:57:31 +0530

    Step 7: Implemented food system with random spawning

--- game.c
+++ game.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<windows.h>
 #include<conio.h>
+#include<time.h>
 #define RIGHT 1
 #define LEFT 2
 #define UP 3
@@ -11,12 +13,17 @@ int main(){
    int height=10;
    int x=width/2; //snake column
    int y=height/2; //snake row
+   srand(time(0));
+   int foodX=rand() % (width-2)+1;
+   int foodY=rand() % (height-2)+1;
    while(1){ //game loop
       system("cls");
    for(int i=0;i<height;i++){
     for(int j=0;j<width;j++){
-        if(i==y&&j==x)
+        if(i==y && j==x)
         printf("O");//snake head
+        else if(i==foodY && j==foodX)
+        printf("@");
         else if(i==0||j==0||i==height-1||j==width-1)
         printf("#"); //border
         else
@@ -27,13 +34,13 @@ int main(){
    
 if(kbhit()){ //keyboard movement
    char ch=getch();
-   if(ch=='d')
+   if(ch=='d'&& dir !=LEFT)
    dir=RIGHT;
-   else if(ch=='a')
+   else if(ch=='a' && dir !=RIGHT)
    dir=LEFT;
-   else if(ch=='w')
+   else if(ch=='w' && dir !=UP)
    dir=UP;
-   else if(ch=='s')
+   else if(ch=='s' && dir !=DOWN)
    dir=DOWN;
    }
 
@@ -42,13 +49,14 @@ else if(dir == LEFT) x--;
 else if(dir == UP)y--;
 else if(dir == DOWN)y++;
 
-// if(x == width-1)x = 1;
-// if(x==0)x=width-2;
-// if(y == height-1)y = 1;
-// if(y==0)y=height-2;
+if(x==foodX&&y==foodY){
+   foodX=rand() % (width-2)+1;
+   foodY=rand() % (height-2)+1;
+}
+
 if(x == 0 || x == width-1 || y == 0 || y == height-1){
     printf("GAME OVER\n");
-    sleep(2000);
+    Sleep(2000);
     break;
 }
    Sleep(300);
