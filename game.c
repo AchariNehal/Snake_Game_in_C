cde9cefc1dbf7d9dc65e4fd0747555abb27c1e14
AchariNehal <nehalachari25@gmail.com>
2026-03-22 12:32:13 +0530

    Created game boundary using loops

---
+++ game.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+   int width=20;
+   int height=10;
+   for(int i=0;i<height;i++){
+    for(int j=0;j<width;j++){
+        if(i==0||j==0||i==height-1||j==width-1)
+        printf("#");
+        else{
+            printf(" ");
+        }
+    }
+    printf("\n");
+   }
+   return 0;
+}
