803d356d66479e4531d8960574ce62230afb7648
AchariNehal <nehalachari25@gmail.com>
2026-03-23 20:06:24 +0530

    stage 8:Snake Tail Movement implemented

--- game.c
+++ game.c
@@ -16,6 +16,9 @@ int main(){
    srand(time(0));
    int foodX=rand() % (width-2)+1;
    int foodY=rand() % (height-2)+1;
+   int tailX[100],tailY[100];
+   int length=0;
+  
    while(1){ //game loop
       system("cls");
    for(int i=0;i<height;i++){
@@ -26,11 +29,24 @@ int main(){
         printf("@");
         else if(i==0||j==0||i==height-1||j==width-1)
         printf("#"); //border
-        else
-            printf(" ");
-    }
-    printf("\n");
-   }
+        else{
+            int istail = 0;
+
+            for(int k=0;k<length;k++){
+               if(i==tailY[k] && j==tailX[k]){
+                     printf("o");
+                     istail = 1;
+                     break;
+                   }
+               }
+
+               if(!istail)
+                  printf(" ");
+            }
+        
+      }
+      printf("\n");
+      }
    
 if(kbhit()){ //keyboard movement
    char ch=getch();
@@ -38,20 +54,34 @@ if(kbhit()){ //keyboard movement
    dir=RIGHT;
    else if(ch=='a' && dir !=RIGHT)
    dir=LEFT;
-   else if(ch=='w' && dir !=UP)
+   else if(ch=='w' && dir !=DOWN)
    dir=UP;
-   else if(ch=='s' && dir !=DOWN)
+   else if(ch=='s' && dir !=UP)
    dir=DOWN;
    }
 
+   int prevX=x;
+   int prevY=y;
+   
  if(dir == RIGHT)x++;
 else if(dir == LEFT) x--;
 else if(dir == UP)y--;
 else if(dir == DOWN)y++;
 
-if(x==foodX&&y==foodY){
+if(x==foodX&&y==foodY){ //eating condition
    foodX=rand() % (width-2)+1;
    foodY=rand() % (height-2)+1;
+   length++;
+}
+
+for(int i = length - 1; i > 0; i--){
+    tailX[i] = tailX[i-1];
+    tailY[i] = tailY[i-1];
+}
+
+if(length > 0){
+    tailX[0] = prevX;
+    tailY[0] = prevY;
 }
 
 if(x == 0 || x == width-1 || y == 0 || y == height-1){
